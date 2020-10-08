b11905f569b560fceda676970054f09f172b38c2
Kaya Overholt <overholtkaya@gmail.com>
2020-10-08 01:19:08 -0700

    cleaned up formatting and added comments

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-//holds the hours of operation
+// Global Variables:
+
+// Holds store hours
 var operationHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', 'Daily Location Total'];
-
+// Parent element for each row of the table
 var parentElement = document.getElementById('salesTable');
+// Tracks cookies sold by all stores per hour
+var totalAllStores = 0;
+// Holds totalAllStores for every hour the stores are open
+var totalCookiesHourStores = [];
 
+// Constructor Function: constructs an object for each store
 var CreateStoreObject = function(storeName, minimumCust, maximumCust, salesAvg) {
   this.name = storeName;
   this.minHourlyCustomers = minimumCust;
@@ -13,12 +20,25 @@ var CreateStoreObject = function(storeName, minimumCust, maximumCust, salesAvg) 
   this.cookiesEachHour = [];
 };
 
+// Object creation
+var seattle = new CreateStoreObject('Seattle', 23, 65, 6.3);
+var tokyo = new CreateStoreObject('Tokyo', 3, 24, 1.2);
+var dubai = new CreateStoreObject('Dubai', 11, 38, 3.7);
+var paris = new CreateStoreObject('Paris', 20, 38, 2.3);
+var lima = new CreateStoreObject('Lima', 2, 16, 4.6);
+//Stores all objects:
+var allStores = [ seattle, tokyo, dubai, paris, lima];
+
+// Prototypes:
+
+// Determines a random amount of customers per hour
 CreateStoreObject.prototype.generateHourlyCustomers = function() {
   for(var i = 0; i < operationHours.length; i++){
     this.customersPerHour.push(Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers + 1) + this.minHourlyCustomers));
   }
 };
 
+// Calculates cookies sold based on amount of customers
 CreateStoreObject.prototype.generateCookieArray = function () {
   for(var i = 0; i < operationHours.length; i++){
     var cookies = Math.ceil(this.customersPerHour[i] * this.avgCookies);
@@ -27,18 +47,7 @@ CreateStoreObject.prototype.generateCookieArray = function () {
   }
 };
 
-function generateHeaderRow() {
-  var newRow = document.createElement('tr');
-  parentElement.appendChild(newRow);
-  var emptyRow = document.createElement('th');
-  newRow.appendChild(emptyRow);
-  for(var i = 0; i < operationHours.length; i++){
-    var newCell = document.createElement('th');
-    newCell.textContent = operationHours[i];
-    newRow.appendChild(newCell);
-  }
-}
-
+// Helps build the table:
 CreateStoreObject.prototype.render = function(){
   var newRow = document.createElement('tr');
   parentElement.appendChild(newRow);
@@ -55,51 +64,34 @@ CreateStoreObject.prototype.render = function(){
   newRow.appendChild(addTotal);
 };
 
-
-
-var seattle = new CreateStoreObject('Seattle', 23, 65, 6.3);
-seattle.generateHourlyCustomers();
-seattle.generateCookieArray();
-var tokyo = new CreateStoreObject('Tokyo', 3, 24, 1.2);
-tokyo.generateHourlyCustomers();
-tokyo.generateCookieArray();
-var dubai = new CreateStoreObject('Dubai', 11, 38, 3.7);
-dubai.generateHourlyCustomers();
-dubai.generateCookieArray();
-var paris = new CreateStoreObject('Paris', 20, 38, 2.3);
-paris.generateHourlyCustomers();
-paris.generateCookieArray();
-var lima = new CreateStoreObject('Lima', 2, 16, 4.6);
-lima.generateHourlyCustomers();
-lima.generateCookieArray();
-generateHeaderRow();
-seattle.render();
-tokyo.render();
-dubai.render();
-paris.render();
-lima.render();
-
-
-var allStores = [ seattle, tokyo, dubai, paris, lima];
-var totalAllStores = 0;
-var totalCookiesHourStores = [];
-function getCookieArrayTotals(){
-  for(var i = 0; i < allStores[0].cookiesEachHour.length; i++){
-    for(var j = 0; j < allStores.length; j++){
-      totalAllStores += allStores[j].cookiesEachHour[i];
-    }
-    totalCookiesHourStores.push(totalAllStores);
-    console.log(totalCookiesHourStores);
-    totalAllStores = 0;
+// Functions:
+// Calls the prototypes needed to get data on each object
+function generateData() {
+  for(var i = 0; i < allStores.length; i++){
+    allStores[i].generateHourlyCustomers();
+    allStores[i].generateCookieArray();
   }
 }
 
-getCookieArrayTotals();
+// Generates the first row of the table
+function generateHeaderRow() {
+  var newRow = document.createElement('tr');
+  parentElement.appendChild(newRow);
+  var emptyRow = document.createElement('th');
+  newRow.appendChild(emptyRow);
+  for(var i = 0; i < operationHours.length; i++){
+    var newCell = document.createElement('th');
+    newCell.textContent = operationHours[i];
+    newRow.appendChild(newCell);
+  }
+}
 
+// Renders the final row of the table with the hourly totals for all stores
 function renderTotals(){
   var newRow = document.createElement('tr');
   parentElement.appendChild(newRow);
   var newCell1 = document.createElement('th');
+  newCell1.textContent = ('Totals');
   newRow.appendChild(newCell1);
   for(var i = 0; i < (operationHours.length - 1); i++){
     var newCell = document.createElement('th');
@@ -111,4 +103,28 @@ function renderTotals(){
   newRow.appendChild(addTotal);
 }
 
+// Determines the amount of cookies sold per hour for all stores and stores it
+function getCookieArrayTotals(){
+  // Gets cookies per hour for every store and adds them together, then moves to the next hour
+  for(var i = 0; i < allStores[0].cookiesEachHour.length; i++){
+    for(var j = 0; j < allStores.length; j++){
+      totalAllStores += allStores[j].cookiesEachHour[i];
+    }
+    totalCookiesHourStores.push(totalAllStores);
+    console.log(totalCookiesHourStores);
+    totalAllStores = 0;
+  }
+}
+
+
+
+// Function Calls:
+generateData();
+generateHeaderRow();
+seattle.render();
+tokyo.render();
+dubai.render();
+paris.render();
+lima.render();
+getCookieArrayTotals();
 renderTotals();
